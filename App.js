d6dadb7f72fb63c997c64153d79acf1a6fc2a5c5
Anushuya <51432613+Anushuyaseeni@users.noreply.github.com>
2023-04-19 18:53:03 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import {View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {ThemeProvider} from 'react-native-elements';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import {DrawerStack} from './src/stack/DrawerStack';
+import DrawerStackScreen from './src/stack/DrawerStack';
+import TabNavigation from './src/stack/TabNavigator';
+
 
 
 const App = () => {
@@ -12,12 +15,11 @@ const App = () => {
   return (
     <ThemeProvider>
       <NavigationContainer>
-          <RootStack.Navigator>
-              <RootStack.Screen name='Cashbook' component={DrawerStack}/>
-          </RootStack.Navigator>
+        <TabNavigation/>
       </NavigationContainer>
     </ThemeProvider>
   );
 };
 
 export default App;
+
