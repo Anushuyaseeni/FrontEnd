6b373384ef1775e970e4843df7178d9b00d760ee
Anushuya <51432613+Anushuyaseeni@users.noreply.github.com>
2023-04-19 18:56:09 +0530

    Add files via upload

---
+++ src/components/settings/Setting.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {View, Text} from 'react-native';
+
+const Settings =() => {
+
+    return(
+        <View style={{backgroundColor: '#ffffff', flex: 1}}>
+            <Text>HI</Text>
+        </View> 
+    )
+
+}
+
+export default Settings;
