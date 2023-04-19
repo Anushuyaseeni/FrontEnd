72d74c964fdc786aebd72c9dc680d40635dbc7eb
Anushuya <51432613+Anushuyaseeni@users.noreply.github.com>
2023-04-19 18:55:20 +0530

    Add files via upload

---
+++ src/components/bookDisplay/cashBook.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {View, Text} from 'react-native';
+
+const CashBook =() => {
+
+    return(
+        <View style={{backgroundColor: '#ffffff', flex: 1}}>
+            <Text>HI</Text>
+        </View> 
+    )
+
+}
+
+export default CashBook;
---
+++ src/components/bookDisplay/expenses.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {View, Text} from 'react-native';
+
+const Expenses =() => {
+
+    return(
+        <View style={{backgroundColor: '#ffffff', flex: 1}}>
+            <Text>HI</Text>
+        </View> 
+    )
+
+}
+
+export default Expenses;
