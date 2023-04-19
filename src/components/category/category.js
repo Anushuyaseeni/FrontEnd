9629b042e3237917bb651ceefb6454add3d5d7a8
Anushuya <51432613+Anushuyaseeni@users.noreply.github.com>
2023-04-19 18:55:49 +0530

    Add files via upload

---
+++ src/components/category/category.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {View, Text} from 'react-native';
+
+const Category =() => {
+
+    return(
+        <View style={{backgroundColor: '#ffffff', flex: 1}}>
+            <Text>HI</Text>
+        </View> 
+    )
+
+}
+
+export default Category;
