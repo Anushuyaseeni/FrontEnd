823f9efd0bea3a5b3189ec9b1f5be9cdce3dcdc8
Anushuya <51432613+Anushuyaseeni@users.noreply.github.com>
2023-05-04 18:57:22 +0530

    Update TabNavigator.js
    tab navigation design changes(optimization)

--- src/stack/TabNavigator.js
+++ src/stack/TabNavigator.js
@@ -1,5 +1,4 @@
 import React, {useRef} from 'react';
-import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import FIcon from 'react-native-vector-icons/Foundation';
 import IonIcon from 'react-native-vector-icons/Ionicons';
@@ -11,44 +10,33 @@ import Settings from '../components/settings/Setting';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {View, Text, Animated, Easing} from 'react-native';
 
-const Tab = createBottomTabNavigator();
-
-const AnimatedIcon = Animated.createAnimatedComponent(Icon);
-
 const TabNavigation = () => {
+  const Tab = createBottomTabNavigator();
   // fadeAnim will be used as the value for opacity. Initial Value: 0
   const fadeAnim = useRef(new Animated.Value(0)).current;
-
-  // const fadeIn = () => {
-  //   // Will change fadeAnim value to 1 in 5 seconds
-  //   Animated.timing(fadeAnim, {
-  //     toValue: 1,
-  //     duration: 500,
-  //     useNativeDriver: true,
-  //   }).start();
-  // };
+  const animVal = useRef(new Animated.Value(0)).current;
 
   const fadeIn = () => {
+    // console.log('dnewiewqdoiwendio');
     Animated.loop(
       // runs the animation array in sequence
       Animated.sequence([
         // shift element to the left by 2 units
         Animated.timing(fadeAnim, {
-          toValue: -60,
-          duration: 50,
-          // easing: Easing.bounce,
+          toValue: -10,
+          duration: 600,
           useNativeDriver: true,
         }),
         // shift element to the right by 2 units
-        // Animated.timing(fadeAnim, {
-        //   toValue: 80,
-        //   duration: 50,
-        //   useNativeDriver: true,
-        // }),
-         // bring the element back to its original position
         Animated.timing(fadeAnim, {
-          toValue: -20,
-          duration: 50,
+          toValue: 0,
+          duration: 600,
+          useNativeDriver: true,
+        }),
+        // bring the element back to its original position
+        Animated.timing(fadeAnim, {
+          toValue: -10,
+          duration: 600,
           useNativeDriver: true,
         }),
       ]),
@@ -57,9 +45,24 @@ const TabNavigation = () => {
     ).start();
   };
 
+  const parallelAnim = () => {
+    Animated.parallel([
+      Animated.timing(animVal,{
+        toValue:-1,
+        duration:1000,
+        useNativeDriver:true
+      }),
+      Animated.timing(animVal,{
+        toValue:0,
+        duration:1000,
+        useNativeDriver:true
+      })
+    ]),{iterations:1}
+  }
+
   return (
     <Tab.Navigator
-      screenOptions={{
+      screenOptions={({route}) => ({
         headerShown: false,
         tabBarStyle: {
           backgroundColor: '#7FC4DD',
@@ -74,246 +77,65 @@ const TabNavigation = () => {
         tabBarInactiveTintColor: '#f0eded',
         tabBarShowLabel: true,
         tabBarLabelPosition: 'below-icon',
-      }}
+        tabBarIcon: ({color}) => {
+          let icon;
+          if (route.name == 'Graph') {
+            icon = 'graph-pie';
+            return (
+              <Animated.View style={{transform: [{translateX: animVal}]}}>
+                <FIcon name={icon} color={color} size={27} style={{marginLeft:-20}} />
+              </Animated.View>
+            );
+          } else if (route.name == 'Setting') {
+            icon = 'ios-settings-outline';
+            return (
+              // <Animated.View style={{transform: [{translateX: fadeAnim}]}}>
+                <IonIcon name={icon} color={color} size={25} />
+              // </Animated.View>
+            );
+          } else if (route.name == 'Expenses') {
+            icon = 'credit-card-plus-outline';
+            return (
+              <Icon
+                name={icon}
+                color={color}
+                size={25}
+                style={{
+                  borderRadius: 90,
+                  height: 60,
+                  width: 60,
+                  backgroundColor: '#7FC4DD',
+                  textAlign: 'center',
+                  marginBottom: 50,
+                  paddingTop: 16,
+                  shadowColor: 'black',
+                  borderWidth: 8,
+                  borderColor: '#ffffff',
+                  position: 'relative',
+                }}
+              />
+            );
+          } else if(route.name == 'Home'){
+            icon = 'home'
+            return (
+              <Animated.View style={{transform: [{translateX: fadeAnim}]}}>
+                <Icon name={icon} color={color} size={25} />
+              </Animated.View>
+            );
+          } else if(route?.name == 'Category') {
+            icon = 'view-grid-plus'
+            return(
+              <Icon name={icon} color={color} size={25}/>
+            )
+          }
+        },
+      })}
       initialRouteName="Home">
-      <Tab.Screen
-        name="Home"
-        component={Home}
-        options={{
-          tabBarLabel: ({focused}) =>
-            focused ? (
-              <Text
-                style={{marginBottom: 20, fontSize: 12, fontWeight: 'bold'}}>
-                Home
-              </Text>
-            ) : (
-              ''
-            ),
-          tabBarLabelStyle: {marginBottom: 20, fontWeight: 'bold'},
-          tabBarIconStyle: {marginTop: 15},
-          tabBarIcon: ({color, focused}) =>
-            focused ? (
-              <Animated.View style={{transform: [{translateY: fadeAnim}]}}>
-                <Icon
-                  name="home"
-                  color={color}
-                  size={25}
-                  style={{
-                    borderRadius: 90,
-                    height: 60,
-                    width: 60,
-                    backgroundColor: '#7FC4DD',
-                    textAlign: 'center',
-                    marginBottom: 50,
-                    paddingTop: 16,
-                    shadowColor: 'black',
-                    borderWidth: 8,
-                    borderColor: '#ffffff',
-                    position: 'relative',
-                  }}
-                />
-              </Animated.View>
-            ) : (
-              <Animated.View style={{transform: [{translateY: fadeAnim}]}}>
-              <Icon
-                name="home"
-                color={color}
-                size={25}
-                style={{marginTop: -30}}
-                onPress={fadeIn}
-              />
-              </Animated.View>
-            ),
-        }}
-      />
-      <Tab.Screen
-        name="Graph"
-        component={CashBook}
-        options={{
-          tabBarLabel: ({focused}) =>
-            focused ? (
-              <Text
-                style={{marginBottom: 20, fontSize: 12, fontWeight: 'bold'}}>
-                Graph
-              </Text>
-            ) : (
-              ''
-            ),
-          tabBarLabelStyle: {marginBottom: 20, fontWeight: 'bold'},
-          tabBarIconStyle: {marginTop: 15},
-          tabBarIcon: ({color, focused}) =>
-            focused ? (
-              <FIcon
-                name="graph-pie"
-                color={color}
-                size={27}
-                style={{
-                  borderRadius: 90,
-                  height: 60,
-                  width: 60,
-                  backgroundColor: '#7FC4DD',
-                  textAlign: 'center',
-                  marginBottom: 50,
-                  paddingTop: 16,
-                  shadowColor: 'black',
-                  borderWidth: 8,
-                  borderColor: '#ffffff',
-                  position: 'relative',
-                }}
-              />
-            ) : (
-              <FIcon
-                name="graph-pie"
-                color={color}
-                size={27}
-                style={{marginTop: -30}}
-              />
-            ),
-        }}
-      />
-      <Tab.Screen
-        name="Expenses"
-        component={Expenses}
-        options={{
-          tabBarLabel: ({focused}) =>
-            focused ? (
-              <Text
-                style={{marginBottom: 20, fontSize: 12, fontWeight: 'bold'}}>
-                Expenses
-              </Text>
-            ) : (
-              ''
-            ),
-          tabBarIconStyle: {marginTop: 15},
-          tabBarLabelStyle: {marginBottom: 20, fontWeight: 'bold'},
-          tabBarIcon: ({color, focused}) =>
-            focused ? (
-              <Icon
-                name="credit-card-plus-outline"
-                color={color}
-                size={25}
-                style={{
-                  borderRadius: 90,
-                  height: 60,
-                  width: 60,
-                  backgroundColor: '#7FC4DD',
-                  textAlign: 'center',
-                  marginBottom: 50,
-                  paddingTop: 16,
-                  shadowColor: 'black',
-                  borderWidth: 8,
-                  borderColor: '#ffffff',
-                  position: 'relative',
-                }}
-              />
-            ) : (
-              <Icon
-                name="credit-card-plus-outline"
-                color={color}
-                size={25}
-                style={{
-                  marginTop: -30,
-                }}
-              />
-            ),
-        }}
-      />
-      <Tab.Screen
-        name="Category"
-        component={Category}
-        options={{
-          tabBarLabel: 'Category',
-          tabBarLabel: ({focused}) =>
-            focused ? (
-              <Text
-                style={{marginBottom: 20, fontSize: 12, fontWeight: 'bold'}}>
-                Category
-              </Text>
-            ) : (
-              ''
-            ),
-          tabBarLabelStyle: {marginBottom: 20, fontWeight: 'bold'},
-          tabBarIconStyle: {marginTop: 15},
-          tabBarIcon: ({color, focused}) =>
-            focused ? (
-              <Icon
-                name="view-grid-plus"
-                color={color}
-                size={25}
-                style={{
-                  borderRadius: 90,
-                  height: 60,
-                  width: 60,
-                  backgroundColor: '#7FC4DD',
-                  textAlign: 'center',
-                  marginBottom: 50,
-                  paddingTop: 16,
-                  shadowColor: 'black',
-                  borderWidth: 8,
-                  borderColor: '#ffffff',
-                  position: 'relative',
-                }}
-              />
-            ) : (
-              <Icon
-                name="view-grid-plus"
-                color={color}
-                size={25}
-                style={{
-                  marginTop: -30,
-                }}
-              />
-            ),
-        }}
-      />
-      <Tab.Screen
-        name="Setting"
-        component={Settings}
-        options={{
-          tabBarLabel: 'Setting',
-          tabBarLabel: ({focused}) =>
-            focused ? (
-              <Text
-                style={{marginBottom: 20, fontSize: 12, fontWeight: 'bold'}}>
-                Setting
-              </Text>
-            ) : (
-              ''
-            ),
-          tabBarLabelStyle: {marginBottom: 19, fontWeight: 'bold'},
-          tabBarIconStyle: {marginTop: 15},
-          tabBarIcon: ({color, focused}) =>
-            focused ? (
-              <IonIcon
-                name="ios-settings-outline"
-                color={color}
-                size={25}
-                style={{
-                  borderRadius: 90,
-                  height: 60,
-                  width: 60,
-                  backgroundColor: '#7FC4DD',
-                  textAlign: 'center',
-                  marginBottom: 50,
-                  paddingTop: 16,
-                  shadowColor: 'black',
-                  borderWidth: 8,
-                  borderColor: '#ffffff',
-                  position: 'relative',
-                }}
-              />
-            ) : (
-              <IonIcon
-                name="ios-settings-outline"
-                color={color}
-                size={25}
-                style={{
-                  marginTop: -30,
-                }}
-              />
-            ),
-        }}
-      />
+      <Tab.Screen name="Home" component={Home} listeners={({}) => ({ tabPress: e => { fadeIn(); }, focus: e => { fadeIn(); }, })} />
+      <Tab.Screen name="Graph" component={CashBook} listeners={({}) => ({tabPress: e => {parallelAnim()}, focus: e => {parallelAnim()} })}/>
+      <Tab.Screen name="Expenses" component={Expenses} />
+      <Tab.Screen name="Category" component={Category} />
+      <Tab.Screen name="Setting" component={Settings} />
     </Tab.Navigator>
   );
 };
