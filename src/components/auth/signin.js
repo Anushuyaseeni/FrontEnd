3546ed50fc37f21e1f1d2672b991f55569054846
Anushuya <51432613+Anushuyaseeni@users.noreply.github.com>
2023-06-09 13:03:44 +0530

    Update signin.js

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Button, View, Text } from 'react-native';
 
 const Signin = () => {
+  console.log('Test branch request');
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
